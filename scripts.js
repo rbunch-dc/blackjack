de09a15db285b4c1aa708179d0c446ff1c12ae75
Robert Bunch <robert.bunch@tinroofsoftware.com>
2015-10-15 16:23:45 -0400

    Black Jack
    Black Jack, v2

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
+var deck = [];
+var placeInDeck = 0;
+var playerTotalCards = 2;
+var dealerTotalCards = 2;
+var playerHand;
+var dealerHand;
 
 
-function deal(){
+function shuffleDeck(){
 	var deck =[];
 	//fill our deck, in order (for now)
 	//suit 
@@ -22,7 +28,7 @@ function deal(){
 	}
 	console.log(deck);
 
-	// var numberOfTimesToShuffle = Math.floor( Math.random() * 500 + 500);
+	var numberOfTimesToShuffle = Math.floor( Math.random() * 500 + 500);
 	var numberOfTimesToShuffle = 2000;
 
 // Math.random() // Create a random 16 digit number between 0 and 1
@@ -42,8 +48,92 @@ function deal(){
 	}
 	//Shuffled Deck
 	console.log(deck);
+	return deck;
+}
+
+function placeCard(card, who, slot){
+	var currId = who + '-card-' + slot;
+	document.getElementById(currId).className = "card";
+	document.getElementById(currId).innerHTML = card;
+
+}
+
+function bust(who){
+	if(who === "player"){
+		//player lost!!! Dealer won!!!
+		document.getElementById('message').innerHTML = "You have busted! Better luck next time!"
+	}else{
+		document.getElementById('message').innerHTML = "The dealer has busted! You won!"
+	}
+}
+
+function calculateTotal(hand, who){
+	var total = 0;
+	for(i=0; i<hand.length; i++){
+		var cardValue = Number(hand[i].slice(0, -1));
+		total = total + cardValue;
+		// total += cardValue;
+	}
+	var idWhoToGet = who + '-total';
+	document.getElementById(idWhoToGet).innerHTML = total;
+
+	//check for bust
+	if(total > 21){
+		bust(who);
+	}
+
+}
+
+function deal(){
+	//Shuffled deck from function shuffleDeck
+	deck = shuffleDeck();
+	playerHand = [ deck[0], deck[2] ];
+	dealerHand = [ deck[1], deck[3] ];
+	placeInDeck = 4;
+
+	placeCard(playerHand[0], 'player', 'one');
+	placeCard(dealerHand[0], 'dealer', 'one');
+	placeCard(playerHand[1], 'player', 'two');
+	placeCard(dealerHand[1], 'dealer', 'two');
+
+	calculateTotal(playerHand, 'player');
+	calculateTotal(dealerHand, 'dealer');
+
+}
+
+function hit(){
+	var slot;
+	if(playerTotalCards === 2){ slot = "three";}
+	else if(playerTotalCards === 3){ slot = "four";}
+	else if(playerTotalCards === 4){ slot = "five";}
+	else if(playerTotalCards === 5){ slot = "six";}
+
+	placeCard(deck[placeInDeck],'player',slot);
+	playerHand.push(deck[placeInDeck]);
+	playerTotalCards++;
+	placeInDeck++;
+	calculateTotal(playerHand, 'player');
+
+}
+
+function checkWin(){
+	
 }
 
 function stand(){
 
 }
+
+function setName(){
+	var name = "Rob";
+	return name;
+}
+
+var name = setName();
+
+
+
+
+
+
+
